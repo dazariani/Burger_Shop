0939c3bec21de5c335ca5bcef99a4448673b2e96
dazariani <davitazariani@gmail.com>
2026-03-08 00:22:54 +0400

    Wrap useSearchparams in suspense

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,19 +4,20 @@ import DarkMode from "./DarkMode"
 import LinksDropdown from "./LinksDropdown"
 import Logo from "./Logo"
 import NavSearch from "./NavSearch"
-import UserIcon from "./UserIcon"
+import { Suspense } from "react"
 
 function Navbar() {
   return (
     <nav className='border-b'>
       <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4'>
         <Logo />
-        <NavSearch />
+        <Suspense fallback={<div>Loading...</div>}>
+          <NavSearch />
+        </Suspense>
         <div className='flex gap-4 items-center'>
           <CartButton />
           <DarkMode />
           <LinksDropdown />
-          {/* <UserIcon /> */}
         </div>
       </Container>
     </nav>
